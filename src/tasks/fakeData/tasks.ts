0bcfd501fe855f46449c32cc0d2e860f234c72c2
nikolasmelui <slak@samaradom.ru>
2019-09-29 14:45:45 +0300

    Refactor fake tasks data to new task model

--- src/tasks/fakeData/tasks.ts
+++ src/tasks/fakeData/tasks.ts
@@ -1,17 +1,22 @@
+import { TaskStatus } from '../task.model';
+
 export default [
   {
-    name: 'First task',
+    id: '1',
+    title: 'First task',
     description: 'The first task',
-    category: 'Home',
+    status: TaskStatus.OPEN,
   },
   {
-    name: 'Second task',
+    id: '2',
+    title: 'Second task',
     description: 'The second task',
-    category: 'Work',
+    status: TaskStatus.IN_PROGRESS,
   },
   {
-    name: 'Third task',
+    id: '3',
+    title: 'Third task',
     description: 'The third task',
-    category: 'Events',
+    status: TaskStatus.DONE,
   },
 ];
