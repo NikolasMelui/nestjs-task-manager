09b2968584f7717a2e87333b1a8f685a1be6fc51
nikolasmelui <slak@samaradom.ru>
2019-09-29 15:40:05 +0300

    Add getTaskById logic into service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,6 +12,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto) {
     const { title, description } = createTaskDto;
 
