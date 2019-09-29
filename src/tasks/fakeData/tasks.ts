d9503143e2e24ff2bfcafd415c79f45390051b9b
nikolasmelui <slak@samaradom.ru>
2019-09-29 14:21:05 +0300

    Add fakeData folder. Add tasks to the fake data.

---
+++ src/tasks/fakeData/tasks.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: 'First task',
+    description: 'The first task',
+    category: 'Home',
+  },
+  {
+    name: 'Second task',
+    description: 'The second task',
+    category: 'Work',
+  },
+  {
+    name: 'Third task',
+    description: 'The third task',
+    category: 'Events',
+  },
+];
