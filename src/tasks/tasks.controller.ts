9e4ecc23fa5054b7de645dd155e9a270440eefc5
nikolasmelui <slak@samaradom.ru>
2019-09-30 19:43:58 +0300

    Add comments to separate the import levels

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,3 +1,4 @@
+// Common
 import {
   Controller,
   Get,
@@ -6,18 +7,25 @@ import {
   Param,
   Delete,
   Patch,
+  Query,
 } from '@nestjs/common';
+// Services
 import { TasksService } from './tasks.service';
+// Models
 import { Task, TaskStatus } from './task.model';
+// DTO
 import { CreateTaskDto } from './dto/create-tesk.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getTasks(): Task[] {
-    return this.taskService.getTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    return Object.keys(filterDto).length
+      ? this.taskService.getFilteredTasks(filterDto)
+      : this.taskService.getAllTasks();
   }
 
   @Get('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,15 +1,39 @@
+// Common
 import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import fakeTasks from './fakeData/tasks';
+// Libs
 import * as uuid from 'uuid/v1';
+// Models
+import { Task, TaskStatus } from './task.model';
+// DTO
 import { CreateTaskDto } from './dto/create-tesk.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+import fakeTasks from './fakeData/tasks';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = fakeTasks;
 
-  getTasks(): Task[] {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getFilteredTasks(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        task =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+    return tasks;
   }
 
   getTask(id: string): Task {
