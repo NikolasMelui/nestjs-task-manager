a60ddc983b2768aa8e2547acbd380878475fd6b2
nikolasmelui <slak@samaradom.ru>
2019-10-03 22:24:57 +0300

    Clean up from old unnecessary code

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,9 +11,11 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
   ) {}
+
   // getAllTasks(): Task[] {
   //   return this.tasks;
   // }
+
   // getFilteredTasks(filterDto: GetTasksFilterDto): Task[] {
   //   const { status, search } = filterDto;
   //   let tasks = this.getAllTasks();
@@ -28,6 +30,7 @@ export class TasksService {
   //   }
   //   return tasks;
   // }
+
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
     if (!found) {
@@ -35,26 +38,11 @@ export class TasksService {
     }
     return found;
   }
+
   createTask(createTaskDto: CreateTaskDto) {
     return this.taskRepository.createTask(createTaskDto);
   }
 
-  // createTask(createTaskDto: CreateTaskDto) {
-  //   const { title, description } = createTaskDto;
-  //   const task: Task = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: TaskStatus.OPEN,
-  //   };
-  //   this.tasks.push(task);
-  //   return task;
-  // }
-  // updateTaskStatusById(id: string, status: TaskStatus): Task {
-  //   const task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
   async deleteTaskById(id: number): Promise<number> {
     const result = (await this.taskRepository.delete(id)).affected;
 
