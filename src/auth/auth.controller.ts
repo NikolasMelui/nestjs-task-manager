40d8e813e04d639255a802ffaac5f090ba151dc6
nikolasmelui <slak@samaradom.ru>
2019-10-06 20:38:06 +0300

    Add password validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Injectable } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -7,7 +7,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     this.authService.signUp(authCredentialsDto);
   }
 }
