27e70b6d50766cee9d9b81bdb9739ea2922a478c
nikolasmelui <slak@samaradom.ru>
2019-10-07 21:55:39 +0300

    Set up the JWT and Passport modules into app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [DatabaseModule, TasksModule, AuthModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      // TODO: Fix it in productoin
+      secret: 'nikolassupersecretstring',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    DatabaseModule,
+    TasksModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
