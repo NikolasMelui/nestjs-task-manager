5e78cc97ab0509be2003e31061b2cb2a8d002bf2
nikolasmelui <slak@samaradom.ru>
2019-10-07 22:30:29 +0300

    Remove unnecessary imports from main app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,8 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      // TODO: Fix it in productoin
-      secret: 'nikolassupersecretstring',
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
-    DatabaseModule,
-    TasksModule,
-    AuthModule,
-  ],
+  imports: [DatabaseModule, TasksModule, AuthModule],
 })
 export class AppModule {}
