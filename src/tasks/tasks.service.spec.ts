c7b1a566a7219c17a7669fe8670b0082f0675fff
nikolasmelui <slak@samaradom.ru>
2019-10-22 21:37:30 +0300

    Create testing module and inject providers in it

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,22 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TaskRepository } from './task.repository';
+
+const mockTaskRepository = () => ({});
+
+describe('TasksService', () => {
+  let tasksService;
+  let taskRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTaskRepository },
+      ],
+    }).compile();
+
+    tasksService = await module.get<TasksService>(TasksService);
+    taskRepository = await module.get<TaskRepository>(TaskRepository);
+  });
+});
