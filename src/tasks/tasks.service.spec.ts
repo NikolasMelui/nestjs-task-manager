afb900acd85c1f336ea4a0aabd50765382f7092c
nikolasmelui <slak@samaradom.ru>
2019-10-22 23:46:09 +0300

    Add test on the updateTaskStatus method

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -112,4 +112,26 @@ describe('TasksService', () => {
       );
     });
   });
+
+  describe('updateTaskStatus', () => {
+    it('updates the task status', async () => {
+      const save = jest.fn().mockResolvedValue(true);
+
+      tasksService.getTaskById = jest.fn().mockResolvedValue({
+        status: TaskStatus.OPEN,
+        save,
+      });
+
+      expect(tasksService.getTaskById).not.toHaveBeenCalled();
+      expect(save).not.toHaveBeenCalled();
+      const result = await tasksService.updateTaskStatus(
+        1,
+        TaskStatus.DONE,
+        mockUser,
+      );
+      expect(tasksService.getTaskById).toHaveBeenCalled();
+      expect(save).toHaveBeenCalled();
+      expect(result.status).toEqual(TaskStatus.DONE);
+    });
+  });
 });
